9f4c639efd66353772e072bc49654d7f8a1bd05f
Apollo <erthaljonas@gmail.com>
2017-05-18 11:43:50 -0400

    First commit

---
+++ seal.rb
@@ -0,0 +1,8 @@
+# Sample of the chorus
+
+require 'baby'
+
+I.(you) == "Kiss from a rose on the grey"
+me.inject(:+).you = stanger.feels + "yeeeaaa"
+Now.you('rose').is?('in bloom')
+gloom_on_the_grey.is_hit('a light')
